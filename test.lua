eb7c49f13762f7900eeec6d3c5d25570c1fb8a89
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2022-10-28 05:33:11 +0300

    Add tests

---
+++ test.lua
@@ -0,0 +1,34 @@
+-- test.lua < tests.txt
+
+local escape = require "escape"
+local line = io.lines()
+local methods = {
+	{"unparse", escape.unparse},
+}
+
+local count = 0
+local failures = 0
+
+while true do
+	local test = line()
+	if test == nil or test == "" then break end
+	count = count + 1
+	for _,v in ipairs(methods) do
+		local name, method = unpack(v)
+		local expected = line()
+		local result = method(test)
+		if result ~= expected then
+			print("Failed " .. name)
+			print("Test:     " .. test)
+			print("Expected: " .. expected)
+			print("Result:   " .. result)
+			failures = failures + 1
+		end
+	end
+	-- skip cruft
+	repeat
+		test = line()
+	until test == nil or test == ""
+end
+
+print(count .. " tests, " .. failures .. " failed")
