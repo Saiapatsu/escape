bacf29e8023becc0308081b5ecf75286b4fa9a60
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2022-10-31 05:35:34 +0200

    es6test.js: add methods, trim input, clarify usage

--- es6test.js
+++ es6test.js
@@ -1,15 +1,20 @@
 /*
 Javascript escape function tester.
 Requires an interactive environment with ES6, such as a browser console.
-Execute escape.js, then paste the contents of tests.txt between the `` and run this script.
+Execute escape.js, then replace all \ with \\ in tests.txt, then paste the contents of tests.txt between the `` and run this script.
 */
 
 ((tests)=>{
 const methods = [
 	["unparse", unparse],
+	["unparseDumb", unparseDumb],
+	["argv", escapeArgv],
+	["argvDumb", escapeArgvDumb],
+	["cmd", escapeCmd],
+	["cmdDumb", escapeCmdDumb],
 ]
 var count = 0, failures = 0;
-tests.split("\n\n").map(x => x.split("\n")).forEach(lines => {
+tests.trim().split("\n\n").map(x => x.split("\n")).forEach(lines => {
 	const test = lines[0];
 	methods.forEach(([name, method], index) => {
 		count++;
