e7ce9dbd7b81dce7ba8dcd04cb73c9ade09b49d2
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2022-10-31 03:45:18 +0200

    test.lua: Generate tests

--- test.lua
+++ test.lua
@@ -11,24 +11,56 @@ local methods = {
 	{"unparse", escape.unparse},
 }
 
+-- luvit print can only print to tty
+local print, print2
+function print(...)
+	if select("#", ...) ~= 0 then
+		return print2(...)
+	else
+		io.write("\n")
+	end
+end
+function print2(str, ...)
+	io.write(tostring((str)))
+	if select("#", ...) ~= 0 then -- "if ..." would eat nils
+		io.write("\t")
+		return print2(...)
+	else
+		io.write("\n")
+	end
+end
+
 local count = 0
 local failures = 0
+
+-- true: print test cases
+-- false: test
+local create = true
 
 while true do
 	local test = line()
 	if test == nil or test == "" then break end
-	for _,v in ipairs(methods) do
-		count = count + 1
-		local name, method = unpack(v)
-		local expected = line()
-		local result = method(test)
-		if result ~= expected then
-			print("Failed " .. name)
-			print("Test:     " .. test)
-			print("Expected: " .. expected)
-			print("Result:   " .. result)
-			print()
-			failures = failures + 1
+	if create then
+		print(test)
+		for _,v in ipairs(methods) do
+			local name, method = unpack(v)
+			print(method(test))
+		end
+		print()
+	else
+		for _,v in ipairs(methods) do
+			count = count + 1
+			local name, method = unpack(v)
+			local expected = line()
+			local result = method(test)
+			if result ~= expected then
+				print("Failed " .. name)
+				print("Test:     " .. test)
+				print("Expected: " .. expected)
+				print("Result:   " .. result)
+				print()
+				failures = failures + 1
+			end
 		end
 	end
 	-- skip cruft
@@ -37,4 +69,6 @@ while true do
 	until test == nil or test == ""
 end
 
-print(count .. " tests, " .. failures .. " failed")
+if not create then
+	print(count .. " tests, " .. failures .. " failed")
+end
