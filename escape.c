62891c49f9be7e3b6f4a95620bdf9dd8ffa18cf7
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2023-02-11 19:43:08 +0200

    escape.c: simplify further (still no output buffer length check)

--- escape.c
+++ escape.c
@@ -10,38 +10,24 @@ int argvDumb(
 	*out++ = '\"';
 	for (;;) {
 		if (*in == '\\') {
-			in++;
-			for (int count = 1; ; count++) {
-				if (*in == '\\') {
-					/* more backslashes */
-					in++;
-				} else if (*in == '\"') {
-					/* sequence of backslashes followed by quote, escape everything */
-					while (count) {
-						*out++ = '\\';
-						*out++ = '\\';
-						count--;
-					}
-					*out++ = '\\';
-					break;
-				} else if (*in == '\0') {
-					/* sequence of backslashes at end of string, escape them and the trailing quote in output */
-					while (count) {
-						*out++ = '\\';
-						*out++ = '\\';
-						count--;
-					}
-					*out++ = '\\';
-					goto end;
-				} else {
-					/* sequence of backslashes not followed by quote, the backslashes were literal */
-					while (count) {
-						*out++ = '\\';
-						count--;
-					}
-					break;
-				}
+			/* count and pass backslashes */
+			*out++ = '\\';
+			int count = 1;
+			while (*++in == '\\') {
+				*out++ = '\\';
+				count++;
 			}
+			if (*in == '\"') {
+				/* escape quote and all slashes */
+				count++;
+				while (count--) *out++ = '\\';
+			} else if (*in == '\0') {
+				/* same, as the output string will be capped with a quote anyway */
+				count++;
+				while (count--) *out++ = '\\';
+				break;
+			}
+			/* else backslashes were not followed by anything special and were copied as-is */
 			
 		} else if (*in == '\"') {
 			/* stray quote */
@@ -49,16 +35,17 @@ int argvDumb(
 			
 		} else if (*in == '\0') {
 			/* end of input string */
-			end:
-			*out++ = '\"';
-			*out = '\0';
 			break;
 		}
-		/* else do nothing noteworthy other than the following */
+		
 		*out = *in;
 		in++;
 		out++;
 	}
+	*out++ = '\"';
+	*out = '\0';
+	
+	return 0;
 }
 
 void main() {
