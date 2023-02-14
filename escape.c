92c8683f487eaf3e0cc8b1d7dfcb31d8164dddf9
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2023-02-14 03:33:54 +0200

    escape.c: abandon resuming entirely

--- escape.c
+++ escape.c
@@ -5,63 +5,64 @@ int argvDumb(
 	char *src,
 	char *dst,
 	int  n,
-	char **endstr, /* optional out: pointer to the null-terminator of output */
-	int  *state) /* optional in/out: state to resume from */
+	char **endstr) /* optional out: pointer to the null-terminator of output */
 {
-	char *cap = dst + n; /* first byte after end of dst, cannot write here */
+	char *cap = dst + n - 1; /* for bounds check */
 	char *anchor = src; /* first byte in src that hasn't been written to dst yet */
 	int terminate = 0; /* flag: whether \0 has been encountered */
+	int count;
+	
 	*dst++ = '\"';
 	for (;;) {
-		int count = 0;
-		int toCopy;
+		count = 0;
 		while (*src == '\\') { count++; src++; }
 		/* if this sequence of 0 or more backslashes is followed by a quote
 		.. or a null, emit source characters followed by some amount of
 		.. backslashes and break the loop if necessary */
 		if (*src == '\"') {
-			count++; /* throw in an extra backslash to escape this quote */
+			count++; /* extra backslash to escape this quote */
 		} else if (*src == '\0') {
 			terminate++; /* finish after this iteration */
 		} else {
 			src++;
 			continue;
 		}
-		toCopy = src - anchor;
-		if (dst + toCopy >= cap) {
-			return 1;
-		}
-		memcpy(dst, anchor, toCopy); dst += toCopy; anchor = src;
-		if (dst + count >= cap) {
-			return 1;
-		}
-		memset(dst, '\\', count); dst += count;
-		src++;
+		
+		int toCopy = src - anchor;
+		
+		/* bounds check */
+		if (dst + toCopy + count >= cap) return 1;
+		
+		memcpy(dst, anchor, toCopy);
+		dst += toCopy;
+		memset(dst, '\\', count);
+		dst += count;
+		anchor = src;
+		
+		src++; /* jump over whatever that if/else block found */
 		if (terminate) break;
 	}
-	if (cap - dst < 2) {
-		return 1;
-	}
+	
+	/* because of the -2 in cap's initial value, there will be room for these */
 	*dst++ = '\"';
 	*dst = '\0';
 	if (endstr != NULL) *endstr = dst;
-	
 	return 0;
 }
 
 void main() {
-	char inBuf[] = "\"trailing and\\ in\"\"ternal\\\\\\\" quote\\\\\"";
-	char outBuf[100];
-	char *src = inBuf;
-	char *dst = outBuf;
+	char src[] = "\"trailing and\\ in\"\"ternal\\\\\\\" quote\\\\\"";
+	char dst[70];
+	char example[] = "\"\\\"trailing and\\ in\\\"\\\"ternal\\\\\\\\\\\\\\\" quote\\\\\\\\\\\"\"";
 	char *end;
-	int state;
-	if (argvDumb(src, dst, 51, &end, &state)) {
+	memset(dst, 'x', 70);
+	dst[69] = '\0';
+	int n1 = 51;
+	if (argvDumb(src, dst, n1, &end)) {
 		puts("not enough");
-		return;
 	}
-	puts(inBuf);
-	puts(outBuf);
-	puts("\"\\\"trailing and\\ in\\\"\\\"ternal\\\\\\\\\\\\\\\" quote\\\\\\\\\\\"\"");
+	puts(src);
+	puts(dst);
+	puts(example);
 	if (end == NULL || *end != '\0') puts("*end does not point to \\0");
 }
